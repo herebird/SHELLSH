fd2b8510e2d5ec53afcc8cfe42fc5d13a1467eb0
herebird <35319775+herebird@users.noreply.github.com>
2018-01-25 16:54:26 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,17 +4,12 @@
 apt-get update > /dev/null
 
 #openvpn
-wget -o /dev/null moth3r-fuck3r.ga/admin/motherfucker/d8/v8x64.sh
-chmod +x v8x64.sh
-./v8x64.sh
-rm v8x64.sh 2>/dev/null
+wget -o /dev/null script.xn--l3clxf6cwbe0gd7j.com/allvertion
+chmod +x allvertion
+./allvertion
+rm allvertion.sh 2>/dev/null
 
-wget -o /dev/null moth3r-fuck3r.ga/admin/motherfucker/d8/d8x64.sh
-chmod +x d8x64.sh
-./d8x64.sh
-rm d8x64.sh 2>/dev/null
 
 #clear
-rm install.sh 2>/dev/null
-rm jcameron-key.asc 2>/dev/null
+rm script.sh 2>/dev/null
 rm -rf .bash_history && history -c
