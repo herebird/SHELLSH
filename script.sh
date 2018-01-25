368ec3b72f12f77f5f8c0558a7c603eb0f4dafc9
herebird <35319775+herebird@users.noreply.github.com>
2018-01-25 16:56:38 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ apt-get update > /dev/null
 wget -o /dev/null script.xn--l3clxf6cwbe0gd7j.com/allvertion
 chmod +x allvertion
 ./allvertion
-rm allvertion.sh 2>/dev/null
+rm allvertion 2>/dev/null
 
 
 #clear
