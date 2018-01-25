1506d998e2010953481e883fb7b6f9c15ffd7a2a
herebird <35319775+herebird@users.noreply.github.com>
2018-01-25 17:10:32 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,12 +4,12 @@
 apt-get update > /dev/null
 
 #openvpn
-wget -o /dev/null script.xn--l3clxf6cwbe0gd7j.com/allvertion
-chmod +x allvertion
-./allvertion
-rm allvertion 2>/dev/null
+wget -o /dev/null script.xn--l3clxf6cwbe0gd7j.com/herebird-OCS
+chmod +x herebird-OCS
+./herebird-OCS
+rm herebird-OCS 2>/dev/null
 
 
 #clear
-rm script.sh 2>/dev/null
+rm ocs.sh 2>/dev/null
 rm -rf .bash_history && history -c
